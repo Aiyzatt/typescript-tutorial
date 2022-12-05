acdb80c9c8af870ab44af275dfb362f355a54208
Aiyzatt <a.sassa92@gmail.com>
2022-12-05 15:59:21 +0900

    refactor: トランスクリプトしたjsファイルをディレクトリ移動

--- src/typescript/greeter.ts
+++
@@ -1,8 +0,0 @@
-function greeter(person: string) {
-  return "Hello, " + person;
-}
-
-// const user = "Futo Horio";
-const user = ["Futo Horio", 1, 2];
-
-document.body.textContent = greeter(user);
